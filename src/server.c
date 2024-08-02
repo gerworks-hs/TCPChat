54026de3771971f8a12ac53e4881b956ca1c2adc
itsgerliz <gerworks.hs@gmail.com>
2024-08-02 20:19:46 +0200

    Fix/ Ft/ Program can now catch SIGINT and some fixes have been added

--- src/server.c
+++ src/server.c
@@ -1,32 +1,46 @@
-//Made by Gerworks-HS (@itsgerliz)
+//-->Made by Gerworks-HS (@itsgerliz)
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <poll.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-int main(int argc, char **argv) {
+//Global variables for the file descriptors (serverSocket and clientSocket)
+int serverSocket;
+int clientSocket;
+//Function to handle Control+C (SIGINT)
+void sigint_handler(int signal){
+	close(serverSocket);
+	close(clientSocket);
+	fprintf(stderr, "Control+C(SIGINT) catched\n");
+	exit(EXIT_FAILURE);
+}
+//Start of the main program
+void main(int argc, char **argv) { //Main function
+	//Configure signal handler for SIGINT (Control+C)
+	signal(SIGINT, sigint_handler);
 	//Check for correct app usage
 	if (argc != 2) {
 		fprintf(stdout, "Usage: %s <port>\n", argv[0]);
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 	//Parse port from argv to a long using strtol
 	uint16_t localPort; //Variable for later use
 	long parsedPort = strtol(argv[1], NULL, 10);
 	if (parsedPort < 1 || parsedPort > 65535) { //Check if it overflows or underflows the valid range
 		fprintf(stderr, "Port <%s> is not in a valid range\n", argv[1]);
-		return 1;
+		exit(EXIT_FAILURE);
 	} else {
 		localPort = (uint16_t)parsedPort; //Type-cast the long to an unsigned 16-bit integer (0-65535)
 		fprintf(stdout, "Server will listen on port <%d>\n", localPort);
 	}
 	//Create server local socket
-	const int serverSocket = socket(AF_INET, SOCK_STREAM, 0); //Create server local socket (TCP/IP)
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0); //Create server local socket (TCP/IP)
 	if (serverSocket == -1) { //Check for errors
 		fprintf(stderr, "Failed trying to create server local socket\n");
-		return 1;
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stdout, "Server local socket created, file descriptor is <%d>\n", serverSocket);
 	}
@@ -40,7 +54,7 @@ int main(int argc, char **argv) {
 	if (bind(serverSocket, (const struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
 		fprintf(stderr, "Failed to bind server address to local socket\n");
 		close(serverSocket);
-		return 1;
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stdout, "Server address bound to local socket\n");
 	}
@@ -48,7 +62,7 @@ int main(int argc, char **argv) {
 	if (listen(serverSocket, 5) == -1) {
 		fprintf(stderr, "Error trying to set local socket as listening\n");
 		close(serverSocket);
-		return 1;
+		exit(EXIT_FAILURE);
 	} else {
 		fprintf(stdout, "Local socket is now listening for incoming connections\n");
 	}
@@ -70,6 +84,6 @@ int main(int argc, char **argv) {
 	}
 	close(serverSocket); //Close the server local socket file descriptor
 	fprintf(stdout, "Server local socket closed, shutting down...\n");
-	return 0; //Main return
+	exit(EXIT_SUCCESS); //Main return
 }
-//Made by Gerworks-HS (@itsgerliz)
+//-->Made by Gerworks-HS (@itsgerliz)
